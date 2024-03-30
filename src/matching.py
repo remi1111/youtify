62142adfb108ab17ec2131f66404748aca3c15f7
remco zevering <remco.zevering@gmail.com>
2024-03-30 22:03:59 +0100

    Moved string to date function.

--- src/matching.py
+++ src/matching.py
@@ -1,5 +1,6 @@
 """ Matching of titles artists and videos. """
 import string
+from datetime import date
 
 def string_clean(strs):
     """ Cleans a string to have only a-z and 0-9. """
@@ -13,3 +14,10 @@ def string_clean(strs):
 def compare_string(str1, str2):
     """ Compares 2 strings. """
     return string_clean(str1) == string_clean(str2)
+
+def str_date(strng):
+    """ Transforms a string to date object. """
+    obj = date(int(strng[0:4]),
+                int(strng[5:7]),
+                int(strng[8:10]))
+    return obj
