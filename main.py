6063bd7e1deb678cd954bdf25dfcdc553a8eecc9
remco zevering <remco.zevering@gmail.com>
2024-03-30 22:17:48 +0100

    Added verbose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """ Main Executable. """
 import sys
+import os
 import getopt
 from dotenv import load_dotenv
 import exec_func
@@ -8,7 +9,7 @@ def main():
     """ Main function. """
     load_dotenv() # Load .env file
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hFLD", ["help","to-file","to-list", "--to-dict"])
+        opts, args = getopt.getopt(sys.argv[1:], "hFLDv", ["help","to-file","to-list", "--to-dict", "--verbose"])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err, file=sys.stderr)  # will print something like "option -a not recognized"
@@ -17,6 +18,8 @@ def main():
     for opt, _ in opts:
         if opt in ("-h", "--help"): # Help message
             usage()
+        elif opt in ("-v", "--verbose"):
+            os.environ["VERBOSE"] = "1"
         elif opt in ("-F", "--to-file"):
             if len(args) != 2:
                 print(f"{opt} requires 2 parameters", file=sys.stderr)
@@ -39,6 +42,7 @@ def main():
 def usage():
     """ Prints how to use the file. """
     print("-h, --help: Prints this message.")
+    print("-v, --verbose: Prints important output during runtime.")
     print("-F, --to-file <1> <2>: outputs spotify playlist <1> info to specified file <2>.")
     print("-L, --to-list <1> <2>: outputs spotify playlist <1> as a list of youtube video\
 ID's to specified file <2>.")
