c6c72b07039f646e7f89644e2292b73208ee065a
remco zevering <remco.zevering@gmail.com>
2024-03-30 19:17:28 +0100

    Added some helper functions.

--- src/analyze.py
+++ src/analyze.py
@@ -1,4 +1,8 @@
 """ Analyzing results from API calls. """
+from datetime import date
+
+import matching
+import yt_api
 
 def analyze_list(json_list):
     """ Analyzing JSON result from youtube api call.
@@ -8,22 +12,23 @@ def analyze_list(json_list):
     mylist = []
     for item in json_list:
         count += 1
-        title = item["snippet"]["title"]
-        if 'korean version' in title.lower():
+        orig_title = item["snippet"]["title"]
+        title = item["snippet"]["title"].lower()
+        if 'korean version' in title:
             test += 1
-        elif 'full version' in title.lower():
+        elif 'full version' in title:
             test += 1
-        elif 'remix' in title.lower():
+        elif 'remix' in title:
             continue
-        elif '(inst.)' in title.lower():
+        elif '(inst.)' in title:
             continue
-        elif '(inst)' in title.lower():
+        elif '(inst)' in title:
             continue
-        elif '(instrumental)' in title.lower():
+        elif '(instrumental)' in title:
             continue
-        elif 'version' in title.lower():
+        elif 'version' in title:
             continue
-        elif 'ver.' in title.lower():
+        elif 'ver.' in title:
             continue
         channel = item["snippet"]["channelTitle"]
         videoid = item["snippet"]["resourceId"]["videoId"]
@@ -34,14 +39,14 @@ def analyze_list(json_list):
         try:
             splitted = descrip.split("Released on: ")[1][0:10]
             released_on = splitted
-            mydict = {'song name': title,
+            mydict = {'song name': orig_title,
                       'artist name': channel,
                       'videoid': videoid,
                       "ytlink": ytlink,
                       'published on': publishedon,
                       'release date': released_on}
         except IndexError:
-            mydict = {'song name': title,
+            mydict = {'song name': orig_title,
                       'artist name': channel,
                       'videoid': videoid,
                       "ytlink": ytlink,
@@ -49,3 +54,41 @@ def analyze_list(json_list):
         mylist.append(mydict)
     # print(count)
     return mylist
+
+def get_songlist_artist(data, artist):
+    """ Creates a list of songnames for an artist """
+    songlist = []
+    for song in data:
+        if song['song artist'] == artist:
+            songlist.append(matching.string_clean(song['song name']))
+    return songlist
+
+def str_date(strng):
+    """ Transforms a string to date object. """
+    obj = date(int(strng[0:4]),
+                int(strng[5:7]),
+                int(strng[8:10]))
+    return obj
+
+def get_videoid_per_artist(artistchannelid, songlist, artist):
+    """ Creates a dictionary of songs per artist. """
+    ytdata = analyze_list(yt_api.get_video_list(yt_api.get_upload_list(artistchannelid),[]))
+    dupedict = {}
+    for song in ytdata:
+        name = matching.string_clean(song['song name'])
+        if name in songlist:
+            if yt_api.video_is_valid(song['videoid']):
+                dat = str_date(song['published on'])
+                if name not in dupedict:
+                    dupedict[name] = [dat, song['videoid'], artist, song['song name']]
+                elif dat < dupedict[name][0] :
+                    dupedict[name] = [dat, song['videoid'], artist, song['song name']]
+    return dupedict
+
+def songs_not_found(songlist, id_dict):
+    """ Creates a list of songnames that were not found for an artist """
+    not_found_list = []
+    for song in songlist:
+        if song not in id_dict.keys():
+            not_found_list.append(song)
+    return not_found_list
--- src/matching.py
+++ src/matching.py
@@ -1,8 +1,5 @@
 """ Matching of titles artists and videos. """
-from datetime import date
 import string
-import yt_api
-import analyze
 
 def string_clean(strs):
     """ Cleans a string to have only a-z and 0-9. """
@@ -13,35 +10,6 @@ def string_clean(strs):
             .replace(" ","") \
             .translate(str.maketrans('', '', string.punctuation))
 
-def get_songlist_artist(data, artist):
-    """ Creates a list of songnames for an artist """
-    songlist = []
-    for song in data:
-        if song['song artist'] == artist:
-            songlist.append(string_clean(song['song name']))
-    return songlist
-
-def get_videoid_per_artist(artistchannelid, songlist, artist):
-    """ Creates a dictionary of songs per artist. """
-    ytdata = analyze.analyze_list(yt_api.get_video_list(yt_api.get_upload_list(artistchannelid),[]))
-    dupedict = {}
-    for song in ytdata:
-        name = string_clean(song['song name'])
-        if name in songlist:
-            if yt_api.video_is_valid(song['videoid']):
-                dat = date(int(song['published on'][0:4]),
-                           int(song['published on'][5:7]),
-                           int(song['published on'][8:10]))
-                if name not in dupedict:
-                    dupedict[name] = [dat, song['videoid'], artist, song['song name']]
-                elif dat < dupedict[name][0] :
-                    dupedict[name] = [dat, song['videoid'], artist, song['song name']]
-    return dupedict
-
-def songs_not_found(songlist, id_dict):
-    """ Creates a list of songnames that were not found for an artist """
-    not_found_list = []
-    for song in songlist:
-        if song not in id_dict.keys():
-            not_found_list.append(song)
-    return not_found_list
+def compare_string(str1, str2):
+    """ Compares 2 strings. """
+    return string_clean(str1) == string_clean(str2)
