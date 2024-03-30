d48dbc114a1224064605e2cfd88291c878c9bf61
remco zevering <remco.zevering@gmail.com>
2024-03-31 00:25:29 +0100

    Commented main in metadata.py

--- src/metadata.py
+++ src/metadata.py
@@ -2,6 +2,7 @@
 import eyed3
 
 def tag_file(filename, mydict, genre):
+    """ Tags a song with info from mydict. """
     audiofile = eyed3.load(filename)
 
     audiofile.tag.artist = mydict['artist name']
@@ -17,17 +18,16 @@ def tag_file(filename, mydict, genre):
 
     audiofile.tag.save()
 
-
-if __name__ == "__main__":
-    filename = "./audio/YOUNG POSSE - ROTY.mp3"
-    mydict = {'artist name': 'YOUNG POSSE',
-              'album name': 'XXL EP',
-              'album artist': 'YOUNG POSSE',
-              'song name spotify': 'ROTY',
-              'release year': '2024',
-              'track num': '4',
-              'track total': '5',
-              'disc num': '1',
-              'disc total': '1'}
-    genre = "K-pop"
-    tag_file(filename, mydict, genre)
+# if __name__ == "__main__":
+#     filename = "./audio/YOUNG POSSE - ROTY.mp3"
+#     mydict = {'artist name': 'YOUNG POSSE',
+#               'album name': 'XXL EP',
+#               'album artist': 'YOUNG POSSE',
+#               'song name spotify': 'ROTY',
+#               'release year': '2024',
+#               'track num': '4',
+#               'track total': '5',
+#               'disc num': '1',
+#               'disc total': '1'}
+#     genre = "K-pop"
+#     tag_file(filename, mydict, genre)
