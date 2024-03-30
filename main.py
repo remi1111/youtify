ce27b7b2f1e50fb1121ec9af9267b00a0df5c6a0
remco zevering <remco.zevering@gmail.com>
2024-03-30 19:17:13 +0100

    Added argument parsing to main()

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
 """ Main Executable. """
+import sys
+import getopt
 from dotenv import load_dotenv
 import exec_func
 
-if __name__ == "__main__":
+def main():
+    """ Main function. """
     load_dotenv() # Load .env file
-    PLAYLIST = "0hxTtbe3Kxll0hKBZR09LV"
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hFLD", ["help","to-file","to-list", "--to-dict"])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(err, file=sys.stderr)  # will print something like "option -a not recognized"
+        usage()
+        sys.exit(3)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"): # Help message
+            usage()
+        elif opt in ("-F", "--to-file"):
+            if len(args) != 2:
+                print(f"{opt} requires 2 parameters", file=sys.stderr)
+                sys.exit(3)
+            exec_func.playlist_to_file(args[0], args[1])
+        elif opt in ("-L", "--to-list"):
+            if len(args) != 2:
+                print(f"{opt} requires 2 parameters", file=sys.stderr)
+                sys.exit(3)
+            exec_func.playlist_to_yt_list(args[0], args[1])
+        elif opt in ("-D", "--to-dict"):
+            if len(args) != 2:
+                print(f"{opt} requires 2 parameters", file=sys.stderr)
+                sys.exit(3)
+            exec_func.playlist_to_yt_dict(args[0], args[1])
+        else:
+            print("unhandled option", file=sys.stderr)
+            sys.exit(-1)
 
-    exec_func.playlist_to_file(PLAYLIST)
+def usage():
+    """ Prints how to use the file. """
+    print("-h, --help: Prints this message.")
+    print("-F, --to-file <1> <2>: outputs spotify playlist <1> info to specified file <2>.")
+    print("-L, --to-list <1> <2>: outputs spotify playlist <1> as a list of youtube video ID's to specified file <2>.")
+    print("-D, --to-dict <1> <2>: outputs spotify playlist <1> as a dictionary of relevant info and their \n\t\t\t youtube video ID's to specified file <2>.")
+
+if __name__ == "__main__":
+    main()
+
+    # PLAYLIST = "0hxTtbe3Kxll0hKBZR09LV"
+    # test_playlist = "10GvxKrWBqtUDD8PtRUnsJ"
+
+    # exec_func.playlist_to_file(PLAYLIST)
--- src/exec_func.py
+++ src/exec_func.py
@@ -3,9 +3,18 @@ import spotify_api
 # import yt_api
 
 
-def playlist_to_file(playlist):
+def playlist_to_file(playlist, filename):
     """ Turns a spotify playlist into a file.
         Dictionary of info is from get_playlist """
     token = spotify_api.get_token()
     mydict = spotify_api.get_playlist(playlist, token, [])
-    spotify_api.write_to_file(mydict, "myplaylist.txt")
+    spotify_api.write_to_file(mydict, filename)
+
+def playlist_to_yt_list(playlist, filename=None):
+    """ Turns a spotify playlist into a list of youtube video ids.
+        If no filename is given print to console. """
+
+def playlist_to_yt_dict(playlist, filename=None):
+    """ Turns a spotify playlist into a dictionary.
+        This dictionary contains relevant info and their youtube ids.
+        If no filename is given print to console. """
