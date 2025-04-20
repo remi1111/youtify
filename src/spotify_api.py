936a214ee0ce029b534d8613ff957f7b40b1b9bb
Remco Zevering <remco.zevering@gmail.com>
2025-04-20 23:15:39 +0200

    Classified spotify_api

--- src/spotify_api.py
+++ src/spotify_api.py
@@ -6,81 +6,119 @@ import sys
 
 import requests
 
-def get_token():
-    """ Retrieve spotify api token """
-    # Retrieve ID and secret from .env
-    client_id = os.getenv("SPOTIFY_CLIENT_ID")
-    client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
-    # Authorization
-    url = "https://accounts.spotify.com/api/token"
-    headers = {}
-    data = {}
+class SongData:
+    """ SongData class stores important song data. """
+    album_artist = None
+    album_name = None
+    release_date = None
+    release_year = None
+    song_artist = None
+    song_name = None
+    track_num = None
+    disc_num = None
+    track_total = None
+    disc_total = None
 
-    # Encode as Base64
-    message = f"{client_id}:{client_secret}"
-    message_bytes = message.encode('ascii')
-    base64_bytes = base64.b64encode(message_bytes)
-    base64_message = base64_bytes.decode('ascii')
+    def __init__(self, **kwargs):
+        self.album_artist = kwargs.get("album_artist")
+        self.album_name = kwargs.get("album_name")
+        self.release_date = kwargs.get("release_date")
+        self.song_artist = kwargs.get("song_artist")
+        self.song_name = kwargs.get("song_name")
+        self.track_num = kwargs.get("track_num")
+        self.disc_num = kwargs.get("disc_num")
+        self.track_total = kwargs.get("track_total")
+        self.disc_total = kwargs.get("disc_total")
 
-    headers['Authorization'] = f"Basic {base64_message}"
-    data['grant_type'] = "client_credentials"
+        if self.release_date:
+            self.release_year = kwargs.get("release_date")[:4]
 
-    r = requests.post(url, headers=headers, data=data, timeout=10)
-    if r is None:
-        print("Spotify request timed out", file=sys.stderr)
-        sys.exit(101)
+    def __str__(self):
+        return f"{self.song_artist} - {self.song_name}\n" \
+                f"Release Date: {self.release_date}\n" \
+                f"Album: {self.album_artist} - {self.album_name}\n" \
+                f"Track: {self.track_num}/{self.track_total}\n" \
+                f"Disc: {self.disc_num}/{self.disc_total}"
 
-    token = r.json()['access_token']
-    return token
+class PlaylistData:
+    """ Class that retrieves songs from spotify and puts them in song_list. """
+    song_list: list[SongData] = []
+    playlist_id = None
+    playlist_base_url = "https://api.spotify.com/v1/playlists/{}/tracks"
+
+    def __init__(self):
+        self._auth = SpotifyAuth()
+
+    def get_playlist(self, playlist_id, playlist_url=None):
+        """ Retrieve playlist form spotify """
+        if not playlist_url:
+            playlist_url = "https://api.spotify.com/v1/playlists/{}/tracks"
+
+        res = requests.get(url=playlist_url, headers=self._auth._headers, timeout=10)
+        if res is None:
+            print("Spotify request timed out", file=sys.stderr)
+            sys.exit(101)
+
+        json_data = json.loads(res.content)
+        self.analyze_playlist(json_data)
+
+        next_url = json_data.get('next')
+        if next_url:
+            self.get_playlist(playlist_id, next_url)
+
+    def analyze_playlist(self, json_data):
+        """ Stores data for each song in a SongData object. """
+        for song in json_data['items']:
+            album_artist = song['track']['album']['artists'][0]['name']
+            album_name = song['track']['album']['name']
+            release_date = song['track']['album']['release_date']
+            song_artist = song['track']['artists'][0]['name']
+            song_name = song['track']['name']
+            track_num = song['track']['track_number']
+            disc_num = song['track']['disc_number']
+            track_total = song['track']['album']['total_tracks']
+
+            song_data = SongData(album_artist=album_artist, album_name=album_name, release_date=release_date, song_artist=song_artist, song_name=song_name, track_num=track_num, track_total=track_total, disc_num=disc_num, disc_total=1)
+            self.song_list.append(song_data)
+
+    def __str__(self):
+        return self.song_list
 
 
-def get_playlist(playlist_id, token, mylist, playlist_url=None):
-    """ Retrieve playlist form spotify """
-    if not playlist_url:
-        playlist_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
-    headers = {
-        "Authorization": "Bearer " + token
-    }
-    res = requests.get(url=playlist_url, headers=headers, timeout=10)
-    if res is None:
-        print("Spotify request timed out", file=sys.stderr)
-        sys.exit(101)
+class SpotifyAuth:
+    """ Class for authentication on spotify. """
+    _api_token = None
 
-    json_data = json.loads(res.content)
+    def __init__(self):
+        self.get_token()
+        self._headers = {
+            "Authorization": "Bearer " + self._api_token
+        }
 
-    a_list = analyze_playlist(json_data)
-    newlist = mylist + a_list
+    def get_token(self):
+        """ Retrieve spotify api token. """
+        # Retrieve ID and secret from .env
+        client_id = os.getenv("SPOTIFY_CLIENT_ID")
+        client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
-    next_url = json_data['next']
-    if not next_url:
-        return newlist
+        # Authorization
+        url = "https://accounts.spotify.com/api/token"
+        headers = {}
+        data = {}
 
-    return get_playlist(playlist_id, token, newlist, next_url)
+        # Encode as Base64
+        message = f"{client_id}:{client_secret}"
+        message_bytes = message.encode('ascii')
+        base64_bytes = base64.b64encode(message_bytes)
+        base64_message = base64_bytes.decode('ascii')
 
-def analyze_playlist(json_data):
-    """ Stores data for each song in a dictionary. """
-    mylist = []
-    for song in json_data['items']:
-        album_artist = song['track']['album']['artists'][0]['name']
-        album_name = song['track']['album']['name']
-        release_date = song['track']['album']['release_date']
-        release_year = song['track']['album']['release_date'][:4]
-        song_artist = song['track']['artists'][0]['name']
-        song_name = song['track']['name']
-        track_num = song['track']['track_number']
-        disc_num = song['track']['disc_number']
-        track_total = song['track']['album']['total_tracks']
-        disc_total = 1
-        song_dict = {'album artist': album_artist,
-                     'album name': album_name,
-                     'release date': release_date,
-                     'release year': release_year,
-                     'song artist': song_artist,
-                     'song name': song_name,
-                     'track num': track_num,
-                     'track total': track_total,
-                     'disc num': disc_num,
-                     'disc total': disc_total}
-        mylist.append(song_dict)
-    return mylist
+        headers['Authorization'] = f"Basic {base64_message}"
+        data['grant_type'] = "client_credentials"
+
+        r = requests.post(url, headers=headers, data=data, timeout=10)
+        if r is None:
+            print("Spotify request timed out", file=sys.stderr)
+            sys.exit(101)
+
+        self._api_token = r.json()['access_token']
